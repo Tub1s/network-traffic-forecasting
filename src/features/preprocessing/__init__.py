9936bd3c1b3abbcd4e42c004e888949907e9c320
Tub1s <senukiq@gmail.com>
2022-09-01 12:24:25 +0200

    improved data reading functions

--- src/features/preprocessing/__init__.py
+++ src/features/preprocessing/__init__.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 
 from collections import defaultdict
-from typing import List
+from typing import List, Tuple
 
-def get_list_of_datafiles(path: str, sort: bool = True) -> List[str] | List[int]:
+def get_list_of_datapaths(path: str, sort: bool = True) -> List[str]:
     """
     Generate list of datafiles for given path.
 
@@ -21,40 +21,69 @@ def get_list_of_datafiles(path: str, sort: bool = True) -> List[str] | List[int]
 
     if sort:
         list_of_datafiles = sorted([int(x.replace('.txt', '')) for x in list_of_datafiles])
+        list_of_datafiles = [path + str(file) + '.txt' for file in list_of_datafiles]
+
+    else:
+        list_of_datafiles = [path + file for file in list_of_datafiles]
 
     return list_of_datafiles
 
-def load_data(path: str, datafiles: List[str] | List[int]) -> defaultdict(list):
+def load_data(datafiles: List[str], targets: List[Tuple[str, str]]) -> pd.DataFrame:
+    """
+    Function to load input data from data paths.
+
+    Args:
+        datafiles (List[str]): List of files to load
+        targets (List[Tuple[str, str]]): List of pairs of nodes to load
+    Returns:
+        pd.DataFrame: Contains data on network traffic, value at given point in time n, for pairs of nodes
+    """
+
     df = defaultdict(list)
 
-    if datafiles[0] is type(str):
-        for file in tqdm.tqdm(datafiles):
-            temp = np.loadtxt(f"{path}{file}")
-            df['5->8'].append(temp[5][8])
-            df['8->5'].append(temp[8][5])
-            df['5->12'].append(temp[5][12])
-            df['8->12'].append(temp[8][12])
-    
-    else: 
-        for file in tqdm.tqdm(datafiles):
-            temp = np.loadtxt(f"{path}{str(file)}.txt")
-            #temp = np.loadtxt(f"{DATA_PATH}{file}")
-            df['5->8'].append(temp[5][8])
-            df['8->5'].append(temp[8][5])
-            df['5->12'].append(temp[5][12])
-            df['8->12'].append(temp[8][12])
+
+    for file in tqdm.tqdm(datafiles):
+        data = np.loadtxt(file)
+        for pair in targets:
+            df[f"{pair[0]}->{pair[1]}"].append(data[pair[0]][pair[1]])
 
     df = pd.DataFrame.from_dict(df)
 
     return df
 
-def make_train_test_datasets(data: pd.DataFrame, split_point: int) -> pd.DataFrame:
-    train_dataset = data[:split_point]
-    test_dataset = data[split_point:].reset_index().drop('index', axis=1)
+def make_train_test_datasets(df: pd.DataFrame, split_point: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Creates split train/test split of DataFrame for stream of data.
+    Train split contains data points before `split_point` and test split right after.
+
+    Args:
+        data (pd.DataFrame): Previously created DataFrame that contains sequential data
+        split_point (int): Index of datapoint at which train/test split will be created
+
+    Returns:
+        pd.DataFrame: Train and test datasets
+    """    
+    train_dataset = df[:split_point]
+    test_dataset = df[split_point:].reset_index().drop('index', axis=1)
 
     return train_dataset, test_dataset
 
 def split_sequence(sequence: pd.Series, n_input_steps: int = 5, n_output_steps: int = 1) -> np.ndarray:
+    """
+    Splits sequential dataset into subsequences using sliding window.
+    Creates pairs of input-output arrays containing subsequences of desired lengths. 
+    Allows for experiments on both short and long term predicition windows.
+    Example: for every 5 input steps, predict value in the next 5 steps.
+        
+
+    Args:
+        sequence (pd.Series): Original sequential data
+        n_input_steps (int, optional): Length of input subsequence. Defaults to 5.
+        n_output_steps (int, optional): Length of output subsequence. Defaults to 1.
+
+    Returns:
+        np.ndarray: _description_
+    """    
     X, y = list(), list()
     for i in tqdm.tqdm(range(sequence.shape[0])):
         if i + n_input_steps + n_output_steps < len(sequence) + 1:
